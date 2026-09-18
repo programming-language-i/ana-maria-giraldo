6c5cc8eba5257c5d358edd92f5e5672476d357c7
anmagima <anamariagiraldomarin5@gmail.com>
2026-09-17 20:35:26 -0500

    cambio 2

--- code/calcu.py
+++ code/calcu.py
@@ -1,12 +1,18 @@
-def suma(valor1, valor2):
-    return valor1 + valor2
+def suma (a,b):
+    return a + b
 
-def resta():    
-    pass
+def resta (a,b):
+    return a - b
 
-def multi():   
-    pass 
 
-resultado_suma = suma(5, 2)
+def multi (a,b):
+    return a * b
 
-print(resultado_suma)
+
+resultado_suma = suma(1,2)
+resultado_resta = resta(2,3)
+resultado_multi = multi(1,9)
+
+print(f"Resultado suma {resultado_suma}")
+print(f"Resultado resta {resultado_resta}")
+print(f"Resultado multiplicación {resultado_multi}")

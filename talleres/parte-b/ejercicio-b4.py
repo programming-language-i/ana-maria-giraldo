7d7c3a7713f8d25a2c0275de3e7ce360992e4268
anmagima <anamariagiraldomarin5@gmail.com>
2026-09-26 00:21:58 -0500

    corrección ejercicio b4

--- talleres/parte-b/ejercicio-b4.py
+++ talleres/parte-b/ejercicio-b4.py
@@ -3,7 +3,13 @@
 import threading
 
 hilo = threading.Thread(target=print, args=("hola",))
+
 hilo.start()
+
 hilo.join()
+
 print(hilo.is_alive())
-hilo.start()
+
+hilo2 = threading.Thread(target=print, args=("hola",))
+hilo2.start()
+hilo2.join()
